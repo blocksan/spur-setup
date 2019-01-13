aafb89f22dfe834645f3960f7478607649251512
sandy18394 <sandyghosh18394@gmail.com>
2019-01-13 20:02:16 +0400

    get to post api for login

--- src/apicontrollers/AuthController.js
+++ src/apicontrollers/AuthController.js
@@ -3,7 +3,7 @@ module.exports = (BaseController, AuthService)=>{
     return new class AuthController extends BaseController{
         configure(app) {
             super.configure(app);
-            app.get('/api/auth/login', AuthService.login.bind(this));
+            app.post('/api/auth/login', AuthService.login.bind(this));
             app.post('/api/auth/signup', AuthService.createUser.bind(this));
         }
     }
